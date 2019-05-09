28c58d78e82a7914731ad746319f23ace82882c1
Ryoung27 <RRYoung89@gmail.com>
2019-05-08 19:33:20 -0500

    finish basics

--- postgress_offical_tutorial/post_tut.sql
+++ postgress_offical_tutorial/post_tut.sql
@@ -10,3 +10,29 @@ WHERE membercost > 0 and (membercost < (monthlymaintenance/50))
 
 select * from cd.facilities
 where name like '%Tennis%';
+
+SELECT * FROM cd.facilities
+WHERE facid IN(1, 5)
+
+SELECT name,
+case when (monthlymaintenance > 100) then
+'expensive'
+else
+'cheap'
+end as cost
+FROM cd.facilities
+
+select memid, surname, firstname, joindate
+	from cd.members
+	where joindate >= '2012-09-01';
+
+select distinct surname from cd.members order by surname limit 10;
+
+select surname
+	from cd.members
+union
+select name
+	from cd.facilities;
+
+    select max(joindate) as latest
+	from cd.members;
